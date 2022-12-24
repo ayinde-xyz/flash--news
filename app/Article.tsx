03dfb2dc5bcf671ee2d9fa4cc063449f1838a3a7
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2022-12-24 16:55:45 +0100

    third commit

--- app/Article.tsx
+++ app/Article.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReadMoreButton from "./ReadMoreButton";
 import LiveTimeStamp from "./LiveTimestamp";
-import Image from "next/image";
+
 type Props = {
   article: ArticlesEntry;
 };
 
 const Article = ({ article }: Props) => {
   return (
-    //{article.map((article, i) => (
     <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
       {article.urlToImage && (
         <img
