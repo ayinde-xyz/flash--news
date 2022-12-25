74ddf61dd32df92529d616035dafdbcc40ab24a8
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2022-12-25 09:00:48 +0100

    first commit

--- app/NewsList.tsx
+++ app/NewsList.tsx
@@ -8,8 +8,8 @@ type Props = {
 const NewsList = ({ articles }: Props) => {
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 gap-10">
-      {articles && articles.map((article) => (
-        <Article key={article.source?.id} article={article} />
+      {articles.map((article) => (
+        <Article key={article?.title} article={article} />
       ))}
     </main>
   );
