3ce7250019aa6dd76424a2a7d64317601412beba
Abdulrahman Ayinde <ayindeabdulrahman16@gmail.com>
2022-12-26 10:23:22 +0100

    Update NewsList.tsx

--- app/NewsList.tsx
+++ app/NewsList.tsx
@@ -9,7 +9,7 @@ const NewsList = ({ articles }: Props) => {
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 gap-10">
       {articles?.map((article) => (
-        <Article key={article?.title} article={article} />
+        <Article key={article.source?.id} article={article} />
       ))}
     </main>
   );
