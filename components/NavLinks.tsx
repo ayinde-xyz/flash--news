20621b9f3aaddeebd59720691b65863fc7d34742
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2025-11-03 22:59:41 +0100

    added a live tag

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -14,6 +14,16 @@ const NavLinks = () => {
   return (
     <Breadcrumb className="text-xs md:text-sm  pb-10 max-w-6xl mx-auto border-b">
       <BreadcrumbList className="grid grid-cols-4 md:grid-cols-7 md:gap-4 gap-x-0">
+        <li className=" font-bold  text-center text-red-700">
+          <span className="relative">
+            <span className="absolute top-1.5 -left-2.5 flex size-2">
+              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-700 opacity-75"></span>
+              <span className="relative inline-flex size-2 rounded-full bg-red-700"></span>
+            </span>
+            LIVE
+          </span>
+        </li>
+
         {categories.map((category) => (
           <NavLink
             key={category}
